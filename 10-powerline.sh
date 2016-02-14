ed19529cee2180fcd2c0f5b49f51de274d5b33ec
libcrack <devnull@libcrack.so>
2016-02-14 11:51:31 +0100

    Enable bash powerline

--- 10-powerline.sh
+++ 10-powerline.sh
@@ -1,15 +1,21 @@
-### Powerline {{{
+# Powerline {{{
 
-#powerline-daemon -q
-#export TERM="xterm-256color"
-#export POWERLINE_BASH_CONTINUATION=1
-#export POWERLINE_BASH_SELECT=1
-#pl="/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh"
-#[[ -f "$pl" ]] && . "$pl"
+$(which powerline-daemon) > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    pgrep -f "powerline-daemon" > /dev/null 2>&1 \
+        || powerline-daemon -q
 
-#_update_ps1(){
-#    local pl="/usr/lib/python2.7/site-packages/powerline/shell.py"
-#    export PS1="$(python2 $pl $? 2> /dev/null)"
-#}
-#export PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
-#}}}
+    export TERM="xterm-256color"
+    export POWERLINE_BASH_CONTINUATION=1
+    export POWERLINE_BASH_SELECT=1
+    export POWERLINE_BASH_BINDINGS="/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh"
+    [[ -f "$POWERLINE_BASH_BINDINGS" ]] && . "$POWERLINE_BASH_BINDINGS"
+
+    _update_ps1(){
+        local pl="/usr/lib/python2.7/site-packages/powerline/shell.py"
+        export PS1="$(python2 $pl $? 2> /dev/null)"
+    }
+    export PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
+
+# }}}
