b90e40649b9cdd956ae18fd65165dd09253a3d86
libcrack <devnull@libcrack.so>
2016-03-04 21:42:45 +0100

    Added "--no-check-certificate" to wget aliases

--- 50-downloads.sh
+++ 50-downloads.sh
@@ -1,8 +1,8 @@
-# test -f functions.sh && . functions.sh
-# [[ -f ~/.bash_functions ]] && . ~/.bash_functions
+[[ -f ~/.bash_functions ]] && . ~/.bash_functions
 
 # download full html page
-alias wgetdownload='wget -np -nd -E -k -r -l 1 -e robots=off "$1"'
+alias wgetdownload='wget --no-check-certificate -np -nd -E -k -r -l inf -e robots=off "$1"'
+
 
 # download mp3 from youtube
 youtubedownload(){
@@ -21,7 +21,7 @@ wget-site-grabber(){
     [[ -z "$1" ]] \
         && error "$FUNCNAME <URL>" \
         && return
-    wget \
+   wget --no-check-certificate \
         --recursive \
         --no-clobber \
         --page-requisites \
@@ -30,5 +30,7 @@ wget-site-grabber(){
         --restrict-file-names=windows \
         --domains "${1%%/*}" \
         --no-parent \
+        -l inf \
         "$1"
 }
+
