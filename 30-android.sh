ee482eeede4918a5d9312b45c47d8146c71b62a1
libcrack <devnull@libcrack.so>
2016-03-14 04:25:12 +0100

    Minor static changes to 30-android.sh

--- 30-android.sh
+++ 30-android.sh
@@ -1,8 +1,6 @@
-#
 # Description: Android SDK NDK config
 # Author: devnull@libcrack.so
 # Date: Mon mar 16 03:49:30 CET 2015
-#
 
 export ANDROID_HOME="${HOME}/Android"
 export ANDROID_NDK_ROOT="${ANDROID_HOME}/ndk"
@@ -13,7 +11,7 @@ export ANDROID_PLATFORM_TOOLS="${ANDROID_SDK_ROOT}/platform-tools"
 #export ANDROID_TOOLCHAIN="${ANDROID_NDK_ROOT}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin"
 export ANDROID_TOOLCHAIN="${ANDROID_HOME}/toolchain/bin"
 
-# # Available tool chains at ${ANDROID_NDK_ROOT}/toolchains
+# Available tool chains at ${ANDROID_NDK_ROOT}/toolchains {{{
 # ${ANDROID_NDK_ROOT}/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86_64/bin
 # ${ANDROID_NDK_ROOT}/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86_64/bin
 # ${ANDROID_NDK_ROOT}/toolchains/llvm-3.3/prebuilt/linux-x86_64/bin
@@ -26,23 +24,38 @@ export ANDROID_TOOLCHAIN="${ANDROID_HOME}/toolchain/bin"
 # ${ANDROID_NDK_ROOT}/toolchains/arm-linux-androideabi-clang3.3/prebuilt/linux-x86_64/bin
 # ${ANDROID_NDK_ROOT}/toolchains/mipsel-linux-android-4.8/prebuilt/linux-x86_64/bin
 # ${ANDROID_NDK_ROOT}/toolchains/x86-4.6/prebuilt/linux-x86_64/bin
+# }}}
 
-# # GDB Server (choose just one depending on the targeted Android ABI)"
+# GDB Server (choose just one depending on the targeted Android ABI) {{{
 # PATH="${$ANDROID_SDK_ROOT}/prebuilt/android-x86/gdbserver:${PATH}"
 # PATH="${ANDROID_SDK_ROOT}/prebuilt/android-arm/gdbserver:${PATH}"
 # PATH="${ANDROID_SDK_ROOT}/prebuilt/android-mips/gdbserver:${PATH}"
+# }}}
 
-# # Android misc tools
+# Android misc tools {{{
 # PATH="${ANDROID_SDK_ROOT}/platform-tools/systrace:${PATH}"
 # PATH="${ANDROID_SDK_ROOT}/prebuilt/common/scan-view:${PATH}"
 # PATH="${ANDROID_SDK_ROOT}/prebuilt/common/scan-build:${PATH}"
 # PATH="${ANDROID_SDK_ROOT}/tools/proguard/bin:${PATH}"
+# }}}
 
-# Android tools, platform tools and toolchain
+# Android PATH (tools, platform tools and toolchain) {{{
 export PATH="${ANDROID_NDK_ROOT}:${PATH}"
 export PATH="${ANDROID_TOOLS}:${PATH}"
 export PATH="${ANDROID_PLATFORM_TOOLS}:${PATH}"
-# export PATH="${ANDROID_TOOLCHAIN}:${PATH}"
-export PATH="${PATH}:${ANDROID_TOOLCHAIN}"
+# cd ${ANDROID_TOOLCHAIN}
+# rm python python2
+# mv python2.7 arm-linux-androideabi-python2.7
+# ln -s arm-linux-androideabi-python2.7 arm-linux-androideabi-python
+# ln -s arm-linux-androideabi-python2.7 arm-linux-androideabi-python2
+export PATH="${ANDROID_TOOLCHAIN}:${PATH}"
+# }}}
 
+# Android Studio {{{
+export ANDROID_STUDIO="${HOME}/AndroidStudio"
+# }}}
+
+# Android shell aliases {{{
 alias adbforward="adb forward tcp:31415 tcp:31415"
+# }}}
+
