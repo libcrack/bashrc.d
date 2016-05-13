d142454956de342812cacc6ab5584ebe9a205192
libcrack <devnull@libcrack.so>
2016-05-13 14:02:49 +0200

    Mod: Minor to google helpers

--- 60-google.sh
+++ 60-google.sh
@@ -1,11 +1,10 @@
-
 google() {
     local google_url='https://www.google.de/search?tbs=li:1&q=';
     local q="$@";
-    local agent="Mozilla/4.0";
-    stream=$(curl -A "$agent" -skLm 10 "${google_url}${q//\ /+}" \
+    local agent="Mozilla/4.0"
+    local stream="$(curl -A "$agent" -skLm 10 "${google_url}${q//\ /+}" \
         | grep -a -oP '\/url\?q=.+?&amp' \
-        | sed 's|/url?q=||; s|&amp||');
-    echo -e "${stream//\%/\x}";
+        | sed 's|/url?q=||; s|&amp||')"
+    printf "${stream//\%/\x}\n"
 }
 
